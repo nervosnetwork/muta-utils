0db1e5f4eb131594440c408859b17bf997147464
Eason <kaoimin@qq.com>
2020-05-06 11:13:11 +0800

    change report buffer

--- muta_apm/src/lib.rs
+++ muta_apm/src/lib.rs
@@ -8,7 +8,7 @@ pub use rustracing_jaeger::span::StartSpanOptions;
 use rustracing_jaeger::Tracer;
 
 const SPAN_CHANNEL_SIZE: usize = 20;
-const REPORTER_BUFFER_SIZE: usize = 5;
+const _REPORTER_BUFFER_SIZE: usize = 5;
 
 pub struct MutaTracer {
     pub inner: Tracer,
@@ -20,15 +20,10 @@ impl MutaTracer {
 
         std::thread::spawn(move || {
             let reporter = JaegerCompactReporter::new("muta-chian").unwrap();
-            let mut reporter_buffer = Vec::with_capacity(REPORTER_BUFFER_SIZE);
 
             loop {
                 if let Ok(finished_span) = span_rx.try_recv() {
-                    reporter_buffer.push(finished_span);
-                    if reporter_buffer.len() == REPORTER_BUFFER_SIZE {
-                        reporter.report(&reporter_buffer).unwrap();
-                        reporter_buffer.clear();
-                    }
+                    reporter.report(&vec![finished_span]).unwrap();
                 }
             }
         });
